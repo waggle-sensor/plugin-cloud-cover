d72882cd8ec6207022afcd974fd0785a1040d2f4
Yongho Kim <ironyonghokim@gmail.com>
2022-01-31 12:43:08 -0600

    Added plugin.timtit and cleaned up some

--- app.py
+++ app.py
@@ -8,89 +8,59 @@ import numpy as np
 from unet_module import Unet_Main
 from unet_module import dewarping
 
-import waggle.plugin as plugin
+import waggle.plugin as Plugin
 from waggle.data.vision import Camera
 
 TOPIC_CLOUDCOVER = "env.coverage.cloud"
 
-plugin.init()
-
 
 def run(args):
-    timestamp = time.time()
-    plugin.publish(TOPIC_CLOUDCOVER, 'Cloud Cover Estimator: Start', timestamp=timestamp)
-    print(f"Starts at time: {timestamp}")
-    timestamp = time.time()
-    plugin.publish(TOPIC_CLOUDCOVER, 'Cloud Cover Estimator: Loading Model', timestamp=timestamp)
-    print(f"Loading Model at time: {timestamp}")
-    unet_main = Unet_Main()
-    timestamp = time.time()
-    plugin.publish(TOPIC_CLOUDCOVER, 'Cloud Cover Estimator: Model Loaded', timestamp=timestamp)
-    print(f"Model Loaded at time: {timestamp}")
-    sampling_countdown = -1
-    if args.sampling_interval >= 0:
-        print(f"Sampling enabled -- occurs every {args.sampling_interval}th inferencing")
-        sampling_countdown = args.sampling_interval
+    print("Cloud cover estimation starts...")
+    with Plugin() as plugin, Camera(args.stream) as camera:
+        timestamp = time.time()
+        print(f"Loading Model at time: {timestamp}")
+        with plugin.timeit("plugin.duration.loadmodel"):
+            unet_main = Unet_Main()
 
-    timestamp = time.time()
-    plugin.publish(TOPIC_CLOUDCOVER, 'Cloud Cover Estimator: Loading an Image', timestamp=timestamp)
-    print(f"Loading an Image at time: {timestamp}")
-    # print("Cloud cover estimation starts...")
-    camera = Camera(args.stream)
-    #camera = Camera()
-    while True:
-        sample = camera.snapshot()
-        image = sample.data
-        imagetimestamp = sample.timestamp
-        #image = cv2.imread('image.jpg')
-        #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        #imagetimestamp = time.time()
-        timestamp = time.time()
-        plugin.publish(TOPIC_CLOUDCOVER, 'Cloud Cover Estimator: an Image Loaded', timestamp=timestamp)
-        print(f"Image Loaded at time: {timestamp}")
-
-        if args.debug:
-            s = time.time()
-        timestamp = time.time()
-        plugin.publish(TOPIC_CLOUDCOVER, 'Cloud Cover Estimator: Starting Preprocessing', timestamp=timestamp)
-        print(f"Starting Preprocessing at time: {timestamp}")
-        h, w, c = image.shape
-        image = dewarping(image, h, w)
-        timestamp = time.time()
-        plugin.publish(TOPIC_CLOUDCOVER, 'Cloud Cover Estimator: End Preprocessing', timestamp=timestamp)
-        print(f"End Preprocessing at time: {timestamp}")
-        timestamp = time.time()
-        plugin.publish(TOPIC_CLOUDCOVER, 'Cloud Cover Estimator: Starting Inference and Postprocessing', timestamp=timestamp)
-        print(f"Inference and postprocessing Starts at time: {timestamp}")
-        ratio, hi = unet_main.run(image, out_threshold=args.threshold)
-        timestamp = time.time()
-        plugin.publish(TOPIC_CLOUDCOVER, 'Cloud Cover Estimator: End Inference and Postprocessing', timestamp=timestamp)
-        print(f"End Inference and postprocessing at time: {timestamp}")
-        if args.debug:
-            e = time.time()
-            print(f'Time elapsed for inferencing: {e-s} seconds')
-        timestamp = time.time()
-        plugin.publish(TOPIC_CLOUDCOVER, ratio, timestamp=imagetimestamp)
-        print(f"Cloud coverage: {ratio} at time: {imagetimestamp}")
-        cv2.imwrite('cloudresult.jpg', hi)
-        print('saved')
-        plugin.upload_file('cloudresult.jpg')
-        timestamp = time.time()
-        print(f"End plugin at time {timestamp}")
-
-        if sampling_countdown > 0:
-            sampling_countdown -= 1
-        elif sampling_countdown == 0:
-            sample.save('sample.jpg')
-            plugin.upload_file('sample.jpg')
-            print("A sample is published")
-            # Reset the count
+        sampling_countdown = -1
+        if args.sampling_interval >= 0:
+            print(f"Sampling enabled -- occurs every {args.sampling_interval}th inferencing")
             sampling_countdown = args.sampling_interval
 
-        if args.interval > 0:
-            time.sleep(args.interval)
+        while True:
+            with plugin.timeit("plugin.duration.input"):
+                sample = camera.snapshot()
+                image = sample.data
+                imagetimestamp = sample.timestamp
+                #image = cv2.imread('image.jpg')
+                #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                #imagetimestamp = time.time()
+            if args.debug:
+                s = time.time()
+            with plugin.timeit("plugin.duration.preprocessing"):
+                h, w, c = image.shape
+                image = dewarping(image, h, w)
+            with plugin.timeit("plugin.duration.inferencing"):
+                ratio, inferenced_image = unet_main.run(image, out_threshold=args.threshold)
+            if args.debug:
+                e = time.time()
+                print(f'Time elapsed for inferencing: {e-s} seconds')
+            plugin.publish(TOPIC_CLOUDCOVER, ratio, timestamp=imagetimestamp)
 
-        exit(0)
+            if sampling_countdown > 0:
+                sampling_countdown -= 1
+            elif sampling_countdown == 0:
+                sample.save('sample.jpg')
+                plugin.upload_file('sample.jpg')
+                print("A sample is published")
+                # Reset the count
+                sampling_countdown = args.sampling_interval
+
+            if args.continuous:
+                if args.interval > 0:
+                    time.sleep(args.interval)
+            else:
+                exit(0)
 
 
 if __name__ == '__main__':
@@ -99,6 +69,10 @@ if __name__ == '__main__':
         '-debug', dest='debug',
         action='store_true', default=False,
         help='Debug flag')
+    parser.add_argument(
+        '-continuous', dest='continuous',
+        action='store_true', default=False,
+        help='Flag to run indefinitely')
     parser.add_argument(
         '-stream', dest='stream',
         action='store', default="camera",
